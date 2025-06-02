18ebf01e0d5e4c3908992cd770473d847936c3f3
JoseTorres <jtorresjimenez2002@gmail.com>
2025-06-02 13:03:07 -0600

    Act

--- src/api/books.ts
+++ src/api/books.ts
@@ -1,3 +1,5 @@
+import { Book } from "../types/book";
+
 export async function fetchBooksByAuthors(authors: string[]) {
   const query = authors.map(a => `inauthor:${a}`).join('+');
   const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=20`);
